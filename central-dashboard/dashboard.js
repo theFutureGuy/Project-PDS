b547f0e84cb31cee5c8d19a1af6e6a9f9e82d596
vak-01 <78302047+vak-01@users.noreply.github.com>
2022-08-06 13:34:37 +0530

    Added central dashboard files
    committed directly to the main branch

---
+++ central-dashboard/dashboard.js
@@ -0,0 +1,98 @@
+/* globals Chart:false, feather:false */
+
+(() => {
+  'use strict'
+
+  feather.replace({ 'aria-hidden': 'true' })
+
+  // Graphs
+  const ctx = document.getElementById('myChart')
+  // eslint-disable-next-line no-unused-vars
+  const myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July'
+      ],
+      datasets: [{
+        data: [
+          1504,
+          1603,
+          1590,
+          1780,
+          1790,
+          1560,
+          1660
+        ],
+        lineTension: 0,
+        backgroundColor: 'transparent',
+        borderColor: '#007bff',
+        borderWidth: 4,
+        pointBackgroundColor: '#007bff'
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: false
+          }
+        }]
+      },
+      legend: {
+        display: false
+      }
+    }
+  })
+
+  const ct = document.getElementById('myChart1')
+
+  const myChart1 = new Chart(ct, {
+    type: 'line',
+    data: {
+      labels: [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July'
+      ],
+      datasets: [{
+        data: [
+          1004,
+          987,
+          1011,
+          1021,
+          993,
+          911,
+          900
+        ],
+        lineTension: 0,
+        backgroundColor: 'transparent',
+        borderColor: '#007bff',
+        borderWidth: 4,
+        pointBackgroundColor: '#007bff'
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: false
+          }
+        }]
+      },
+      legend: {
+        display: false
+      }
+    }
+  })
+})()
